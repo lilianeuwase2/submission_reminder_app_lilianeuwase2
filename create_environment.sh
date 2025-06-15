52bebb549735cf3689c221b07ae7f15985030a4e
lilianeuwase2 <l.uwase@alustudent.com>
2025-06-15 10:35:17 -0700

    new changes

--- create_environment.sh
+++ create_environment.sh
@@ -19,28 +19,6 @@ mkdir -p "$main_dir/config"
 echo "directory structure created"
 #creating and populating the files
 
-echo "Creating config/config.env"
-
-cat << EOL > "$main_dir/config/config.env"
-ASSIGNMENT="Shell Navigation"
-DAYS_REMAINING=2
-EOL
-
-echo "Creating assets/submissions.txt"
-cat << EOL > "$main_dir/assets/submissions.txt"
-student, assignment, sustatus
-Chinemerem, Shell Navigation, not submitted
-Chiagoziem, Git, submitted
-Divine, Shell Navigation, not submitted
-Anissa, Shell Basics, submitted
-Keilla, Git, submitted
-Ruth, Shell navigation, not submitted
-Patty, Git, not submitted
-Mutoni, Shell basics, not submitted
-Justine, Shell navigation, not submitted
-Winnie, Shell navigation, not submitted
-EOL
-
 echo "Creating app/reminder.sh"
 cat << EOL > "$main_dir/app/reminder.sh"
 #!/bin/bash
@@ -83,15 +61,47 @@ function check_submissions {
 }
 EOL 
 
+echo "Creating assets/submissions.txt"
+cat << EOL > "$main_dir/assets/submissions.txt"
+student, assignment, sustatus
+Chinemerem, Shell Navigation, not submitted
+Chiagoziem, Git, submitted
+Divine, Shell Navigation, not submitted
+Anissa, Shell Basics, submitted
+Keilla, Git, submitted
+Ruth, Shell navigation, not submitted
+Patty, Git, not submitted
+Mutoni, Shell basics, not submitted
+Justine, Shell navigation, not submitted
+Winnie, Shell navigation, not submitted
+EOL
+
+cat << EOL > "$main_dir/config/config.env"
+# This is the config file
+ASSIGNMENT="Shell Navigation"
+DAYS_REMAINING=2
+EOL 
+
 cat << EOL > "$main_dir/startup.sh"
 #!/bin/bash
-Start the reminder application
-SCRIPT_DIR="$(dirname "$0")"
-bash "$SCRIPT_DIR/reminder.sh"
+ This script initializes and starts the application.
+cd "$(dirname "$0")/../"
+
+# Source the configuration file
+if [ -f "config/config.env" ]; then
+    source "config/config.env"
+else
+    echo "Error: Configuration file not found!"
+    exit 1
+fi
+
+# Run the main reminder script from the 'app' directory
+bash "$main_dir/app/reminder.sh"
 EOL
 
 # Make all .sh scripts executable
-chmod +x "$main_dir/scripts"/*.sh
+find "${main_dir}" -type f -name "*.sh" -exec chmod +x {} \;
+
 
 # Confirm setup complete
 echo "Environment setup complete. Run startup.sh to begin."
